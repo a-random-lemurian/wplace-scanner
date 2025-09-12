f1771203a4abfaa5be32feea0b20affa72fe5148
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2025-09-12 17:49:07 +0800

    INITIAL COMMIT

---
+++ scanner/scanner.go
@@ -0,0 +1,161 @@
+package scanner
+
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/buckhx/tiles"
+	"github.com/rs/zerolog"
+)
+
+// (lat_min, lon_min, lat_max, lon_max)
+type BoundingBox struct {
+	LatMin float64
+	LonMin float64
+	LatMax float64
+	LonMax float64
+}
+
+type WplaceScannerSettings struct {
+	TileServerUrl         string
+	ZoomLevel             int
+	OutputDirectory       string
+	UserAgent             string
+	MaxConcurrentRequests int64
+	Frequency             time.Duration
+	BBox                  BoundingBox
+	HttpClient            *http.Client
+	Log                   *zerolog.Logger
+}
+
+type WplaceScanner struct {
+	settings   *WplaceScannerSettings
+	httpClient *http.Client
+	fetcher    *TileFetcher
+	log        *zerolog.Logger
+
+	tileRequestChannel chan *tiles.Tile
+}
+
+func NewWplaceScanner(settings *WplaceScannerSettings) *WplaceScanner {
+	scanner := &WplaceScanner{
+		settings:           settings,
+		tileRequestChannel: make(chan *tiles.Tile),
+		log:                settings.Log,
+	}
+
+	client := settings.HttpClient
+	if client == nil {
+		client = &http.Client{}
+	}
+
+	fetcher := NewTileFetcher(&TileFetcherSettings{
+		Client:        client,
+		UserAgent:     settings.UserAgent,
+		ServerURL:     settings.TileServerUrl,
+		MaxConcurrent: settings.MaxConcurrentRequests,
+		Log:           settings.Log,
+	})
+
+	scanner.fetcher = fetcher
+
+	return scanner
+}
+
+func (w *WplaceScanner) checkOutputDirectory() error {
+	w.log.Info().
+		Str("outputDirectory", w.settings.OutputDirectory).
+		Msg("Making sure the output directory exists...")
+	return os.MkdirAll(w.settings.OutputDirectory, 0755)
+}
+
+func (w *WplaceScanner) Run() error {
+	w.log.Info().Msg("Starting.")
+	w.log.Debug().Interface("config", w.settings).Msg("Our settings")
+
+	if err := w.checkOutputDirectory(); err != nil {
+		return err
+	} 
+
+	ticker := time.NewTicker(w.settings.Frequency)
+
+	go w.fetcher.Start()
+
+	// Do it the first time so we don't have to wait an entire hour.
+	w.download()
+
+	for range ticker.C {
+		w.download()
+	}
+
+	return nil
+}
+
+func (w *WplaceScanner) download() {
+	zoom := w.settings.ZoomLevel
+
+	northwestTile := tiles.FromCoordinate(
+		w.settings.BBox.LatMin,
+		w.settings.BBox.LonMin,
+		w.settings.ZoomLevel)
+	southeastTile := tiles.FromCoordinate(
+		w.settings.BBox.LatMax,
+		w.settings.BBox.LonMax,
+		w.settings.ZoomLevel)
+
+	minX := min(northwestTile.X, southeastTile.X)
+	maxX := max(northwestTile.X, southeastTile.X)
+	minY := min(northwestTile.Y, southeastTile.Y)
+	maxY := max(northwestTile.Y, southeastTile.Y)
+
+	tileCount := (maxX - minX) * (maxY - minY)
+	w.log.Info().Int("tileCount", tileCount).Msg("Scheduling batch of tile downloads")
+
+	now := time.Now().UTC()
+
+	for x := minX; x <= maxX; x++ {
+		for y := minY; y <= maxY; y++ {
+			w.log.Debug().Int("x", x).Int("y", y).Msg("Scheduling tile download")
+			tile := &tiles.Tile{X: x, Y: y, Z: zoom}
+			go w.getAndWriteTile(tile, now)
+		}
+	}
+}
+
+func (w *WplaceScanner) emptyTile() *WplaceTile {
+	empty1000 := image.NewRGBA(image.Rect(0,0,1000,1000))
+	return &WplaceTile{
+		Image: empty1000,
+	}
+}
+
+func (w *WplaceScanner) getAndWriteTile(tile *tiles.Tile, batchTime time.Time) {
+	receivedTile, err := w.fetcher.FetchTile(tile)
+	if err != nil {
+		w.log.Error().Err(err).Msg("Using empty tile in place of 404 not found tile")
+		receivedTile = w.emptyTile()
+	}
+
+    timeString := batchTime.Format("2006-01-02T15-04-05Z")
+
+	directory := fmt.Sprintf("%s/%s/%d", w.settings.OutputDirectory, timeString, tile.X)
+	err = os.MkdirAll(directory, 0755)
+	if err != nil {
+		w.log.Error().Err(err).Str("dir", directory).Msg("Failed to create directory for tiles")
+		return
+	}
+
+	filename := fmt.Sprintf("%s/%s/%d/%d.png", w.settings.OutputDirectory, timeString, tile.X, tile.Y)
+	file, err := os.Create(filename)
+	if err != nil {
+		w.log.Error().Err(err).Str("file", filename).Msg("Failed to open file for writing")
+		return
+	}
+
+	png.Encode(file, receivedTile.Image)
+	w.log.Debug().Str("file", filename).Msg("Wrote file successfully")
+}
---
+++ scanner/tilefetcher.go
@@ -0,0 +1,158 @@
+package scanner
+
+import (
+	"context"
+	"fmt"
+	"image"
+	"image/png"
+	"net/http"
+	"net/url"
+	"strings"
+	"sync"
+
+	"golang.org/x/sync/semaphore"
+
+	"github.com/buckhx/tiles"
+	"github.com/rs/zerolog"
+	"github.com/spf13/viper"
+)
+
+type TileFetcher struct {
+	cfg                *viper.Viper
+	settings           *TileFetcherSettings
+	log                *zerolog.Logger
+	tileRequestChannel chan *tiles.Tile
+	tileReplies        map[string]chan *WplaceTile
+	tileRepliesMu      sync.Mutex
+}
+
+type TileFetcherSettings struct {
+	Client        *http.Client
+	UserAgent     string
+	ServerURL     string
+	MaxConcurrent int64
+	Log           *zerolog.Logger
+}
+
+type WplaceTile struct {
+	Resp   *http.Response
+	Coords *tiles.Tile
+	Image  image.Image
+	err    error
+}
+
+func NewTileFetcher(settings *TileFetcherSettings) *TileFetcher {
+	return &TileFetcher{
+		settings:           settings,
+		tileRequestChannel: make(chan *tiles.Tile),
+		log:                settings.Log,
+		tileReplies:        make(map[string]chan *WplaceTile),
+	}
+}
+
+func (t *TileFetcher) Start() {
+	go t.tileRequestWorker()
+}
+
+func makeTileKey(tile *tiles.Tile) string {
+	return fmt.Sprintf("%d-%d-%d", tile.X, tile.Y, tile.Z)
+}
+
+func (t *TileFetcher) MakeTileUrl(tile *tiles.Tile) string {
+	base := t.settings.ServerURL
+	withX := strings.ReplaceAll(base, "{x}", fmt.Sprint(tile.X))
+	withY := strings.ReplaceAll(withX, "{y}", fmt.Sprint(tile.Y))
+	withZ := strings.ReplaceAll(withY, "{z}", fmt.Sprint(tile.Z))
+	return withZ
+}
+
+func (t *TileFetcher) FetchTile(tile *tiles.Tile) (*WplaceTile, error) {
+	ch := make(chan *WplaceTile)
+	key := makeTileKey(tile)
+
+	t.tileRepliesMu.Lock()
+	t.tileReplies[key] = ch
+	t.tileRepliesMu.Unlock()
+
+	t.tileRequestChannel <- tile
+
+	receivedTile := <-ch
+
+	if receivedTile.err != nil {
+		return nil, receivedTile.err
+	}
+
+	return receivedTile, nil
+}
+
+func (t *TileFetcher) tileRequestWorker() {
+	var (
+		maxWorkers = t.settings.MaxConcurrent
+		sem        = semaphore.NewWeighted(int64(maxWorkers))
+		ctx        = context.TODO()
+	)
+
+	for tile := range t.tileRequestChannel {
+		if err := sem.Acquire(ctx, 1); err != nil {
+			t.log.Err(err).Msg("Failed to acquire semaphore while queueing tile requests")
+			break
+		}
+
+		go func(tile *tiles.Tile) {
+			defer sem.Release(1)
+			receivedTile, err := t.doTileRequest(tile)
+			if err != nil {
+				t.log.Err(err).Msg("Failed to download tile")
+				receivedTile = &WplaceTile{err: err}
+			}
+
+			t.tileRepliesMu.Lock()
+			ch, ok := t.tileReplies[makeTileKey(tile)]
+			if !ok {
+				t.log.Warn().Msg("Failed to find reply channel for requested tile!")
+			}
+			ch <- receivedTile
+			t.tileRepliesMu.Unlock()
+		}(tile)
+	}
+}
+
+func (t *TileFetcher) doTileRequest(tile *tiles.Tile) (*WplaceTile, error) {
+	client := t.settings.Client
+	req := http.Request{}
+	req.Header = http.Header{}
+	req.Header.Add("user-agent", t.settings.UserAgent)
+	req.Method = http.MethodGet
+
+	parsed, err := url.Parse(t.MakeTileUrl(tile))
+	if err != nil {
+		return nil, err
+	}
+	req.URL = parsed
+
+	t.log.Debug().Stringer("url", req.URL).Msg("Fetching tile URL")
+
+	resp, err := client.Do(&req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to fetch tile: %s", resp.Status)
+	}
+
+	receivedTile := &WplaceTile{}
+	receivedTile.Coords = tile
+
+	img, err := png.Decode(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode image as png: %s", err)
+	}
+	receivedTile.Image = img
+	receivedTile.Resp = resp
+
+	t.log.Trace().Interface("respHeader", resp.Header).Msg("Fetched tile")
+
+	return receivedTile, err
+}
