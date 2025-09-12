7a8380b0d49ed6d648e06cd82e8a545293d6b08c
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2025-09-12 19:16:56 +0800

    refactor(scanner): Refactor tile writing code

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -115,13 +115,20 @@ func (w *WplaceScanner) download() {
 	tileCount := (maxX - minX) * (maxY - minY)
 	w.log.Info().Int("tileCount", tileCount).Msg("Scheduling batch of tile downloads")
 
-	now := time.Now().UTC()
+	
+	batchTime := time.Now().UTC()
+	batchTimeString := batchTime.Format("2006-01-02 15-04-05Z")
+	directory := fmt.Sprintf("%s/%s", w.settings.OutputDirectory, batchTimeString)
+
+	tileMap := &TileMap{}
 
 	for x := minX; x <= maxX; x++ {
 		for y := minY; y <= maxY; y++ {
 			w.log.Debug().Int("x", x).Int("y", y).Msg("Scheduling tile download")
 			tile := &tiles.Tile{X: x, Y: y, Z: zoom}
-			go w.getAndWriteTile(tile, now)
+			receivedTile := w.getTile(tile)
+			tileMap.Add(receivedTile)
+			w.writeTile(receivedTile, directory)
 		}
 	}
 }
@@ -133,23 +140,15 @@ func (w *WplaceScanner) emptyTile() *WplaceTile {
 	}
 }
 
-func (w *WplaceScanner) getAndWriteTile(tile *tiles.Tile, batchTime time.Time) {
-	receivedTile, err := w.fetcher.FetchTile(tile)
-	if err != nil {
-		w.log.Error().Err(err).Msg("Using empty tile in place of 404 not found tile")
-		receivedTile = w.emptyTile()
-	}
-
-    timeString := batchTime.Format("2006-01-02T15-04-05Z")
-
-	directory := fmt.Sprintf("%s/%s/%d", w.settings.OutputDirectory, timeString, tile.X)
-	err = os.MkdirAll(directory, 0755)
+func (w *WplaceScanner) writeTile(receivedTile *WplaceTile, directory string) {
+	subdirectory := fmt.Sprintf("%s/%d", directory, receivedTile.Coords.X)
+	err := os.MkdirAll(directory, 0755)
 	if err != nil {
 		w.log.Error().Err(err).Str("dir", directory).Msg("Failed to create directory for tiles")
 		return
 	}
 
-	filename := fmt.Sprintf("%s/%s/%d/%d.png", w.settings.OutputDirectory, timeString, tile.X, tile.Y)
+	filename := fmt.Sprintf("%s/%d.png", subdirectory, receivedTile.Coords.Y)
 	file, err := os.Create(filename)
 	if err != nil {
 		w.log.Error().Err(err).Str("file", filename).Msg("Failed to open file for writing")
@@ -158,4 +157,14 @@ func (w *WplaceScanner) getAndWriteTile(tile *tiles.Tile, batchTime time.Time) {
 
 	png.Encode(file, receivedTile.Image)
 	w.log.Debug().Str("file", filename).Msg("Wrote file successfully")
+}
+
+func (w *WplaceScanner) getTile(tile *tiles.Tile) *WplaceTile {
+	receivedTile, err := w.fetcher.FetchTile(tile)
+	if err != nil {
+		w.log.Error().Err(err).Msg("Using empty tile in place of 404 not found tile")
+		receivedTile = w.emptyTile()
+		receivedTile.Coords = tile
+	}
+	return receivedTile
 }
